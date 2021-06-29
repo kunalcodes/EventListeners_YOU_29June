e2103daafdaf93dd73b1d49f56cc67aa64737e0c
kunalcodes <kunal.chaturvedi75@gmail.com>
2021-06-29 16:43:32 +0530

    Files added

---
+++ app/src/main/java/com/example/event_listeners_you/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.event_listeners_you;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTvSaveInstance;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTvSaveInstance = findViewById(R.id.tvSaveInstance);
+        mTvSaveInstance.setText("Hey Masai");
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("key","Screen Rotation");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String data = savedInstanceState.getString("key");
+        mTvSaveInstance.setText(data);
+    }
+}
